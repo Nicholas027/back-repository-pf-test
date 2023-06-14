a27e562c51509e2f79d61e32ee658b0fd1c67b42
Nicolɑ́s Sɑɑvedɾɑ <79129057+Nicholas027@users.noreply.github.com>
2023-06-14 00:54:09 -0300

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ["http://localhost:3000", "http://localhost:3001", "https://datazotest.netlify.app"];
+const allowedOrigins = ["http://localhost:3000", "http://localhost:3001", "https://datazotest.netlify.app", "https://datazo.vercel.app"];
 
 module.exports = allowedOrigins;
