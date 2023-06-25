d7183920382865f6a9d8f49af8c73d233155279f
Nicolɑ́s Sɑɑvedɾɑ <79129057+Nicholas027@users.noreply.github.com>
2023-06-25 03:39:37 -0300

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
   "https://mail.google.com",
+  "https://datazotest.netlify.app"
 ];
 
 module.exports = allowedOrigins;
